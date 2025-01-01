954a19ad8936e6d40aeefae32f12131ab7273932
Arif Muharam <arifmuharam20@gmail.com>
2025-01-01 08:52:13 +0700

    feat/main/menambahkan component main (masih kosong)

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../style/styles.css";
+
+export default function Main() {
+  return (
+    <main className="main">
+      <h2>Projects</h2>
+    </main>
+  );
+}
