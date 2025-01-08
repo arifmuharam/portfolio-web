207dcc55ae662aedff51b722189d6106d40cfc58
Arif Muharam <arifmuharam20@gmail.com>
2025-01-08 11:27:28 +0700

    feat/main/selesai menambahkan transisi card pada mobile dan desktop layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Card() {
+  const [isActive, setIsActive] = useState(false);
+  const toggleCardContent = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div class="card">
+    <div className="card" onClick={toggleCardContent}>
       <img
         src="https://images.unsplash.com/photo-1731432245362-26f9c0f1ba2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8"
         alt="Gambar Proyek"
       />
-      <div class="card__content">
-        <p class="card__title">Card Title</p>
-        <p class="card__description">
+      <div className={`card__content ${isActive ? "active" : ""}`}>
+        <p className="card__title">Card Title</p>
+        <p className="card__description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco.
